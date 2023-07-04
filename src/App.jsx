32cf543b6160309b0df400ef80fa5b5247c49e80
Francesco Imperiale <desdemus0@gmail.com>
2023-07-04 17:48:50 +0200

    added new stylesheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,97 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import "./App.scss"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  //this will be a component
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className='heading1'>Welcome</h1>
+      <div className='container'>
+        <ul className='NavBar'>
+          <li>Home</li>
+          <li>Products</li>
+          <li>News</li>
+          <li>Contacts</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="wrapper">
+        <div className="row">
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+        </div>
+        <div className="row">
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+        </div>
+        <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+        </div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+        </div>
+        <div className="row">
+          <div className="col-5">col5</div>
+          <div className="col-7">col7</div>
+        </div>
+        <div className="row">
+          <div className="col-12">col12</div>
+        </div>
+        <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-6">col6</div>
+        </div>
+        <div className="row space-between">
+          <div className="col-1">col1</div>
+          <div className="col-1">col1</div>
+        </div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-8">col8</div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1">Col-xs-1</div>
+        </div>
+
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+        <div className="row">
+          <div className="col-xs-6 col-sm-2 col-md-4">A</div>
+          <div className="col-xs-6 col-sm-6 col-md-4">B</div>
+          <div className="col-xs-12 col-sm-4 col-md-4">C</div>
+        </div>
+
+        <div className="row-g">
+          <div className="col-g-1">A</div>
+          <div className="col-g-1">B</div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
